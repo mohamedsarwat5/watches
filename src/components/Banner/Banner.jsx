938c58a537ed5bd1bb5877b814df8bed43b9dfcd
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2025-11-17 16:31:15 +0200

    add layer

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -8,6 +8,9 @@ export default function Banner() {
     return (
 
         <div className=' bg-[#1b1b1b] lg:h-screen h-[400px] flex items-center justify-center text-center w-full relative overflow-hidden'>
+            {/* <img src={layer}
+                className='absolute -top-28  left-0 z-30  '
+                alt="" /> */}
             <img className='absolute lg:w-[600px] w-[250px] -translate-y-4 z-10' src={w2} alt="Watch" />
             <h2 className='font-bold banner text-center mx-auto uppercase text-[18vw] lg:text-[18vw] '>Chronos</h2>
             <img className=' w-full absolute bottom-10 lg:-bottom-32 z-20' src={layer} alt="" />
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,9 @@
 import { motion } from 'framer-motion';
 import React, { useEffect, useRef, useState } from 'react';
 import screen from "/screen.png"
+import layer from '/layer.png';
+import Banner from '../Banner/Banner';
+
 export default function Home() {
     const [loaded, setLoaded] = useState(false)
     const videoRef = useRef(null)
@@ -17,12 +20,13 @@ export default function Home() {
 
     return (
         <>
-            <section className="relative min-h-dvh  w-full">
+            <section className="relative min-h-dvh overflow-hidden w-full">
                 {!loaded && <img
                     src={screen}
                     className='absolute inset-0 w-full h-full object-cover z-0'
                     s />}
                 <div className='absolute inset-0 w-full h-full bg-black/20 z-20' />
+                <img className='absolute lg:-bottom-60 -bottom-[60px]  w-full z-40 right-0' src={layer} alt="" />
                 <video
                     ref={videoRef}
                     src="/vid.mp4"
@@ -39,6 +43,7 @@ export default function Home() {
                     </button>
                 </div>
             </section>
+            <Banner/>
         </>
     );
 }
