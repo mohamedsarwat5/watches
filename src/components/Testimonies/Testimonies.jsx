fa78cfe5c1f25492496a09531dbdcc56c2e008ba
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-01-26 23:16:25 +0200

    fdfdsa

--- src/components/Testimonies/Testimonies.jsx
+++ src/components/Testimonies/Testimonies.jsx
@@ -6,7 +6,7 @@ import TestSlider from '../Slider/Slider';
 export default function Testimonies() {
     return (<>
         <Space />
-        <div className='h-dvh lg:h-fit w-full overflow-hidden bg-main lg:px-32 relative py-4'>
+        <div className='h-[450px] lg:h-fit w-full overflow-hidden bg-main lg:px-32 relative py-4'>
             <Header text={"Testimonies"} desc2={"Voices from those who chose timeless elegance."} desc={"Voices of timeless elegance"} />
 
             <h2 className='font-bold banner whitespace-nowrap text-center mx-auto uppercase text-[11vw] lg:text-[10vw] absolute lg:-top-10 top-0 left-1/2 -translate-x-1/2 '>Testimonies</h2>
