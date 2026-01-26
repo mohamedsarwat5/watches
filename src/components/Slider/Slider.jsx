bba9590af72c442a4e26f50320b58f2dd87ee6a6
mohamedsarwat5 <mohamedelsayed5897@gmail.com>
2026-01-26 23:03:07 +0200

    try to fix

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -13,19 +13,16 @@ export default function TestSlider() {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        arrows: true,
-       responsive: [
+        initialSlide: 0,
+        arrows: false,
+        responsive: [
             {
                 breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
-                    slidesToScroll: 1,
+                    slidesToScroll: 3,
                     infinite: true,
-                    dots: true,
-                    pauseOnHover: false,
-                    centerMode: false,
+                    dots: true
                 }
             },
             {
@@ -33,25 +30,14 @@ export default function TestSlider() {
                 settings: {
                     slidesToShow: 1,
                     slidesToScroll: 1,
-                    initialSlide: 2,
-                    arrows: false,
-
-                    pauseOnHover: false,
-                    centerMode: false,
+                    initialSlide: 2
                 }
             },
             {
                 breakpoint: 480,
                 settings: {
                     slidesToShow: 1,
-                    slidesToScroll: 1,
-                    speed: 500,
-                    infinite: true,
-                    autoplay: true,
-                    pauseOnHover: false,
-                    dots: true,
-                    arrows: false,
-                    centerMode: false,
+                    slidesToScroll: 1
                 }
             }
         ]
@@ -60,7 +46,7 @@ export default function TestSlider() {
 
 
     return (
-        <div className="slider-container">
+        <div className="slider-container ">
             <Slider {...settings} className=''>
                 {Array.from({ length: 6 }).map((_, index) => (
                     <div key={index} className="px-1 ">
@@ -79,7 +65,7 @@ export default function TestSlider() {
                             <p className='mt-4'>“Very comfortable to wear and looks far more expensive than it is. You can really see the attention to detail.”</p>
                             <div className='flex items-center justify-center space-x-2'>
                                 {imgs.map((img, i) => {
-                                    return <img key={i} className='w-[120px]  aspect-square object-cover rounded-lg inline-block mt-4' src={img} alt="" />
+                                    return <img key={i} className='w-[120px]  lg:w-[120px] aspect-square object-cover rounded-lg inline-block mt-4' src={img} alt="" />
                                 })}
                             </div>
                         </div>
